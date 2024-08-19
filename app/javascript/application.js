6e5fc10ac2b9bf1d99961557649ee8c293385f92
Haythem Mejri <haythemmejri05@gmail.com>
2024-08-19 22:30:41 +0100

    Initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,14 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "popper"
+import "bootstrap"
+import "stripe"
+import "controllers"
+//= require jquery
+//= require jquery-ui
+//= require popper
+//= require bootstrap-sprockets
+
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
